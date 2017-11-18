aa56b6a660c21847a04c4bb0c8ec1a2b238efa7a
y13423685271 <y362424>
2017-11-18 21:03:13 +0800

    ini

--- ch7_4/src/main/java/com/wisely/ch7_4/CustomServletContainer.java
+++ ch7_4/src/main/java/com/wisely/ch7_4/CustomServletContainer.java
@@ -3,7 +3,7 @@ package com.wisely.ch7_4;
 import java.util.concurrent.TimeUnit;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
--- ch8_2/src/main/java/com/wisely/support/CustomRepositoryFactoryBean.java
+++ ch8_2/src/main/java/com/wisely/support/CustomRepositoryFactoryBean.java
@@ -11,7 +11,16 @@ import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
 public class CustomRepositoryFactoryBean<T extends JpaRepository<S, ID>, S, ID extends Serializable>
-    extends JpaRepositoryFactoryBean<T, S, ID> {// 1
+    extends JpaRepositoryFactoryBean<T, S, ID> {
+
+    /**
+     * Creates a new {@link JpaRepositoryFactoryBean} for the given repository interface.
+     *
+     * @param repositoryInterface must not be {@literal null}.
+     */
+    public CustomRepositoryFactoryBean(Class<? extends T> repositoryInterface) {
+        super(repositoryInterface);
+    }// 1
 
     @Override
     protected RepositoryFactorySupport createRepositoryFactory(EntityManager entityManager) {// 2
