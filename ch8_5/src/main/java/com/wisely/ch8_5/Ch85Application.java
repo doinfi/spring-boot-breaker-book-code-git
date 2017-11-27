dd78da51f83220e899b53213e4b00b9d4acbb895
y13423685271 <y362424>
2017-11-27 10:30:41 +0800

    ini

--- ch8_5/src/main/java/com/wisely/ch8_5/Ch85Application.java
+++ ch8_5/src/main/java/com/wisely/ch8_5/Ch85Application.java
@@ -1,6 +1,5 @@
 package com.wisely.ch8_5;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
