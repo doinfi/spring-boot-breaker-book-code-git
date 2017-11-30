e0fb05d8b68599e737ba1d2adc38aca72c546cd5
y13423685271 <y362424>
2017-12-01 00:39:00 +0800

    ini

--- ch6_2_3/src/main/java/com/wisely/ch6_2_3/config/AuthorSettings.java
+++ ch6_2_3/src/main/java/com/wisely/ch6_2_3/config/AuthorSettings.java
@@ -1,28 +1,14 @@
 package com.wisely.ch6_2_3.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "author") //1 
+@ConfigurationProperties(prefix = "author") //1
+@Data
 public class AuthorSettings {
 
     private String name;
     private Long age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public void setAge(Long age) {
-        this.age = age;
-    }
 }
--- ch6_2_3/src/test/java/com/wisely/ch6_2_3/Ch623ApplicationTests.java
+++ ch6_2_3/src/test/java/com/wisely/ch6_2_3/Ch623ApplicationTests.java
@@ -2,13 +2,11 @@ package com.wisely.ch6_2_3;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Ch623Application.class)
-@WebAppConfiguration
+@SpringBootTest(classes = Ch623Application.class)
 public class Ch623ApplicationTests {
 
     @Test
