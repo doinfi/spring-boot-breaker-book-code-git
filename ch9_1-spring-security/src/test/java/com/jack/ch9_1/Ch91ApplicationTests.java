dbd0d686e96c91eca5982afbd529a1f5662a9190
y13423685271 <y362424>
2017-12-07 23:23:43 +0800

    ini

--- ch9_1-spring-security/src/test/java/com/jack/ch9_1/Ch91ApplicationTests.java
+++ ch9_1-spring-security/src/test/java/com/jack/ch9_1/Ch91ApplicationTests.java
@@ -2,12 +2,12 @@ package com.jack.ch9_1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Ch91Application.class)
+@SpringBootTest(classes = Ch91Application.class)
 @WebAppConfiguration
 public class Ch91ApplicationTests {
 
