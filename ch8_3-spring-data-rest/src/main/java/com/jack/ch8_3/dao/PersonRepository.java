72cb052d67f681ef905e9ca5c903f72731ba6649
y13423685271 <y362424>
2017-12-07 19:16:39 +0800

    ini

--- ch8_3-spring-data-rest/src/main/java/com/jack/ch8_3/dao/PersonRepository.java
+++ ch8_3-spring-data-rest/src/main/java/com/jack/ch8_3/dao/PersonRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+/**
+ * @author yangyueming
+ */
 @RepositoryRestResource(path = "people")
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
---
+++ ch8_3-spring-data-rest/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (name, age, address) VALUES ('汪云飞', 32, '合肥');
+INSERT INTO person (name, age, address) VALUES ('xx', 31, '北京');
+INSERT INTO person (name, age, address) VALUES ('yy', 30, '上海');
+INSERT INTO person (name, age, address) VALUES ('zz', 29, '南京');
+INSERT INTO person (name, age, address) VALUES ('aa', 28, '武汉');
+INSERT INTO person (name, age, address) VALUES ('bb', 27, '合肥');
