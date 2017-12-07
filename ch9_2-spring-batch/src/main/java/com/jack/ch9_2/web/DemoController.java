d01b51ddf435452ab637b66546d39ff77e1f8683
y13423685271 <y362424>
2017-12-07 23:59:32 +0800

    ini

--- ch9_2-spring-batch/src/main/java/com/jack/ch9_2/web/DemoController.java
+++ ch9_2-spring-batch/src/main/java/com/jack/ch9_2/web/DemoController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     public JobParameters jobParameters;
+
     @Autowired
     JobLauncher jobLauncher;
     @Autowired
